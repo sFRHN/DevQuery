6680648fe3d526b4c3d8b66d2cbe007eb74f5638
Farhaan <farhaan654@gmail.com>
2025-04-02 21:21:48 -0600

    Landing Page setup with login and registration components

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,10 +1,16 @@
-function LandingPage() {
+import Login from "../Components/Login/Login";
+import Registration from "../Components/Registration/Registration";
+import "./LandingPage.css";
+
+export default function LandingPage() {
 	return (
-		<>
-			<h1>Welcome to the system!</h1>
-			<p>Hello</p>
-		</>
+		<div className="container">
+			<div className="title">My Project</div>
+			<div className="info-box">Welcome to the website</div>
+			<div className="login-register">
+				<Login />
+				<Registration />
+			</div>
+		</div>
 	);
 }
-
-export default LandingPage;
