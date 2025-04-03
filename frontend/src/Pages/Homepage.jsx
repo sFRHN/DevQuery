5d85c880c7a5ee17bb62124ae307894859ac37ad
Farhaan <farhaan654@gmail.com>
2025-04-03 10:11:24 -0600

    Homepage updated - React state variable to maintain currently selected channel - Now loads 2 containers - Left sidebar is the channel list - Right contetn area is the post list for that channel

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,9 +1,27 @@
+import ChannelList from "../Components/ChannelList/ChannelList";
 import PostList from "../Components/Posts/PostList";
+import { useState } from "react";
+import "./Homepage.css";
 
 function HomePage() {
+	const [selectedChannel, setSelectedChannel] = useState(null);
+
 	return (
-		<div>
-			<PostList />
+		<div className="container">
+			<div className="title">Homepage</div>
+			<div className="channel-sidebar">
+				<ChannelList
+					selectedChannel={selectedChannel}
+					setSelectedChannel={setSelectedChannel}
+				/>
+			</div>
+			<div className="content-area">
+				{selectedChannel ? (
+					<PostList channelID={selectedChannel} />
+				) : (
+					<h1>Select a channel</h1>
+				)}
+			</div>
 		</div>
 	);
 }
