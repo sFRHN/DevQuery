4cf802f8b6a02a261a2ecfcfc4d40f52cb8d3875
Farhaan <farhaan654@gmail.com>
2025-04-04 03:08:10 -0600

    Frontend update to handle user authentication

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useAuth } from "../AuthContext.jsx";
 
 export default function Login() {
 	// State to manage form data
@@ -6,11 +7,30 @@ export default function Login() {
 		username: "",
 		password: "",
 	});
+	const { login } = useAuth();
+	const [error, setError] = useState("");
+
+	// Function to handle form submission
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const { username, password } = formData;
+
+		if (!username || !password) {
+			setError("Please enter both username and password");
+		}
+
+		const response = await login(username, password);
+
+		if (!response.success) {
+			setError(response.message || "Login failed");
+		}
+	};
 
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<h1>Login</h1>
-			<label for="username">Username</label>
+			{error && <p className="error-message">{error}</p>}
+			<label htmlFor="username">Username</label>
 			<input
 				type="text"
 				id="username"
@@ -20,7 +40,7 @@ export default function Login() {
 					setFormData({ ...formData, username: e.target.value })
 				}
 			/>
-			<label for="password">Password</label>
+			<label htmlFor="password">Password</label>
 			<input
 				type="text"
 				id="password"
--- frontend/src/Components/Registration/Registration.jsx
+++ frontend/src/Components/Registration/Registration.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
+import { useAuth } from "../AuthContext.jsx";
 
 export default function Registration() {
 	// State to manage form data
 	const [formData, setFormData] = useState({
 		username: "",
+		displayName: "",
 		password: "",
 	});
+	const { register } = useAuth();
+	const [error, setError] = useState("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const { username, displayName, password } = formData;
+
+		if (!username || !password || !displayName) {
+			setError("Please fill in all the fields");
+		}
+
+		const response = await register(username, password, displayName);
+
+		if (!response.success) {
+			setError(response.message || "Registration failed");
+		}
+	};
 
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<h1>Registration</h1>
+			{error && <p className="error-message">{error}</p>}
 			<label for="username">Username</label>
 			<input
 				type="text"
@@ -18,6 +38,16 @@ export default function Registration() {
 				value={formData.username}
 				onChange={(e) =>
 					setFormData({ ...formData, username: e.target.value })
+				}
+			/>
+			<label for="displayName">Display Name</label>
+			<input
+				type="text"
+				id="displayName"
+				name="displayName"
+				value={formData.displayName}
+				onChange={(e) =>
+					setFormData({ ...formData, displayName: e.target.value })
 				}
 			/>
 			<label for="password">Password</label>
